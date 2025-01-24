0cd42c19800c7d3c34ef58f98e92a93f32132256
kourjot <pkour6188@gmail.com>
2025-01-25 01:54:55 +0530

    signup

--- sprint3/day1qus3/app.js
+++ sprint3/day1qus3/app.js
@@ -1,7 +1,10 @@
 import express from "express"
 import "dotenv/config"
 import {db} from "./db.js"
+import {userRouter} from "./router/userRouter.js"
 const app = express()
+app.use(express.json())
+app.use(userRouter)
 app.listen(process.env.PORT,()=>{
     console.log(`Server is running on port ${process.env.PORT}`)
     db()
